870500b05e88c7523574cd6fec4b410c08e2a17a
Dillon Schaffer <dschaffe@calpoly.edu>
2024-10-21 00:59:39 -0700

    Programming Assignment 1

--- hw1.py
+++ hw1.py
@@ -1,28 +1,92 @@
 import data
+from data import Rectangle
+
 
 # Write your functions for each part in the space below.
 
 # Part 1
+def vowel_count(word:str) -> int:
+    count = 0
+    vowels = ['a','e','i','o','u']
+    for letter in word:
+        if letter in vowels:
+            count += 1
+    return count
 
 
 # Part 2
-
+def short_lists(list1:list[list[int]]) -> list:
+    shortl = []
+    for list2 in list1:
+        if len(list2) == 2:
+            shortl.append(list2)
+    return shortl
 
 # Part 3
+def ascending_pairs(list1:list[list[int]])-> list:
+    new_list = []
+    for lists in list1:
+        if len(lists) == 2:
+            temp = ""
+            if lists[0] > lists[1]:
+                temp = lists[1]
+                lists[1] =lists[0]
+                lists[0] = temp
+        new_list.append(lists)
+    return new_list
 
 
 # Part 4
+def add_prices(price1:data.Price, price2:data.Price) -> data.Price:
+    dollar_sum = price1.dollars + price2.dollars
+    cents_sum = price1.cents + price2.cents
+    dollar_remainder = cents_sum//100
+    sum_price = data.Price(dollar_sum + dollar_remainder, cents_sum%100)
+    return sum_price
 
 
 # Part 5
-
+def rectangle_area(rectangle1:data.Rectangle) -> float:
+    x1 = rectangle1.bottom_right.x
+    x2 = rectangle1.top_left.x
+    y1 = rectangle1.top_left.y
+    y2 = rectangle1.bottom_right.y
+    width = x1 - x2
+    length = y1 - y2
+    return width * length
 
 # Part 6
-
+def books_by_author(author:str, books:list[data.Book]) -> list[data.Book]:
+    author_book = []
+    for book in books:
+        if book.authors == author:
+            author_book.append(book)
+    return author_book
 
 # Part 7
-
+def circle_bound(rectangle1:data.Rectangle) -> data.Circle:
+    x1 = rectangle1.bottom_right.x
+    x2 = rectangle1.top_left.x
+    y1 = rectangle1.top_left.y
+    y2 = rectangle1.bottom_right.y
+    width = x1 - x2
+    length = y1 - y2
+    radius = 0
+    if width < length:
+        radius = width / 2
+    else:
+        radius = length / 2
+    center = data.Point((x1 + x2)/2, (y1 + y2)/2)
+    return data.Circle(center, radius)
 
 # Part 8
-
-
+def below_pay_average(all_employees:list[data.Employee]) -> list[str]:
+    sum = 0
+    low_pay_rate_employees = []
+    for employee in all_employees:
+        sum += employee.pay_rate
+    average = sum/len(all_employees)
+    for employee in all_employees:
+        if employee.pay_rate < average:
+            low_pay_rate_employees.append(employee.name)
+    return low_pay_rate_employees
--- hw1_test.py
+++ hw1_test.py
@@ -2,35 +2,128 @@ import data
 import hw1
 import unittest
 
+from data import Rectangle
+
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_vowel_count_1(self):
+        input = "hello"
+        result = hw1.vowel_count(input)
+        expected = 2
+        self.assertEqual(expected,result)
 
+    def test_vowel_count_2(self):
+        input = "onomatopoeia"
+        result = hw1.vowel_count(input)
+        expected = 8
+        self.assertEqual(expected,result)
 
     # Part 2
+    def test_short_lists_1(self):
+        input = [[1, 2], [1, 2, 3], [4, 5], [6]]
+        result = hw1.short_lists(input)
+        expected = [[1,2],[4,5]]
+        self.assertEqual(expected,result)
 
+    def test_short_lists_2(self):
+        input = [[5,1,6],[],[3,67,1,8],[5,1],[-6,8]]
+        result = hw1.short_lists(input)
+        expected = [[5,1],[-6,8]]
+        self.assertEqual(expected,result)
 
     # Part 3
+    def test_ascending_pairs(self):
+        input = [[5,1,6],[5,1],[],[3,67,1,8],[-6,8]]
+        result = hw1.ascending_pairs(input)
+        expected = [[5,1,6],[1,5],[],[3,67,1,8],[-6,8]]
+        self.assertEqual(expected,result)
 
+    def test_ascending_pairs(self):
+        input = [[6,1],[2,6,8,2,3],[2,-1],[5,3,1,6],[-15,5]]
+        result = hw1.ascending_pairs(input)
+        expected = [[1,6],[2,6,8,2,3],[-1,2],[5,3,1,6],[-15,5]]
+        self.assertEqual(expected,result)
 
     # Part 4
+    def test_add_prices_1(self):
+        input1 = data.Price(15, 68)
+        input2 = data.Price(2,12)
+        result = hw1.add_prices(input1, input2)
+        expected = data.Price(17, 80)
+        self.assertEqual(expected, result)
+
+    def test_add_prices_2(self):
+        input1 = data.Price(36, 46)
+        input2 = data.Price(78,79)
+        result = hw1.add_prices(input1, input2)
+        expected = data.Price(115, 25)
+        self.assertEqual(expected, result)
 
 
     # Part 5
+    def test_rectangle_area_1(self):
+        input = data.Rectangle(data.Point(50,200),data.Point(100,100))
+        result = hw1.rectangle_area(input)
+        expected = 5000
+        self.assertEqual(expected, result)
 
+    def test_rectangle_area_2(self):
+        input = data.Rectangle(data.Point(-1000,1000),data.Point(-500,200))
+        result = hw1.rectangle_area(input)
+        expected = 400000
+        self.assertEqual(expected, result)
 
     # Part 6
+    def test_books_by_author_1(self):
+        input1 = "Stephen King"
+        input2 = [data.Book("Stephen King", "IT"), data.Book("J.K. Rowling", "Harry Potter"),
+                  data.Book("Stephen King", "The Shining")]
+        result = hw1.books_by_author(input1, input2)
+        expected = [data.Book("Stephen King", "IT"), data.Book("Stephen King", "The Shining")]
+        self.assertEqual(expected, result)
 
+    def test_books_by_author_2(self):
+        input1 = "William Shakespeare"
+        input2 = [data.Book("William Shakespeare", "Hamlet"), data.Book("William Shakespeare", "Romeo and Juliet"),
+                  data.Book("Stephen King", "The Shining")]
+        result = hw1.books_by_author(input1, input2)
+        expected = [data.Book("William Shakespeare", "Hamlet"), data.Book("William Shakespeare", "Romeo and Juliet")]
+        self.assertEqual(expected, result)
 
     # Part 7
+    def test_circle_bound_1(self):
+        input = data.Rectangle(data.Point(5, 16), data.Point(12, 10))
+        result = hw1.circle_bound(input)
+        expected = data.Circle(data.Point(8.5, 13.0), 3.0)
+        self.assertEqual(expected, result)
 
+    def test_circle_bound_2(self):
+        input = data.Rectangle(data.Point(-10, 56), data.Point(15, 12))
+        result = hw1.circle_bound(input)
+        expected = data.Circle(data.Point(2.5, 34.0), 12.5)
+        self.assertEqual(expected, result)
 
     # Part 8
+    def test_below_pay_average_1(self):
+        input = [data.Employee("Charles", 20000),
+                 data.Employee("Dillon", 50000),
+                 data.Employee("Bobby", 10000),
+                 data.Employee("Jonas", 60000)]
+        result = hw1.below_pay_average(input)
+        expected = ["Charles", "Bobby"]
+        self.assertEqual(expected, result)
 
-
-
+    def test_below_pay_average_1(self):
+        input = [data.Employee("Cash", 30000),
+                 data.Employee("Donnie", 40000),
+                 data.Employee("Lucas", 30000),
+                 data.Employee("Scotty", 30000)]
+        result = hw1.below_pay_average(input)
+        expected = ["Cash", "Lucas", "Scotty"]
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
